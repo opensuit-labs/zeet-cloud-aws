03a390d0e2b2871eaf276d759fa4700003e6626e
Zihao Zhang <z@zeet.co>
2023-09-08 18:02:08 -0700

    Update main.tf
    fix provider version

--- eks/terraform/main.tf
+++ eks/terraform/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.56.0"
+    }
+  }
 }
 
 data "aws_caller_identity" "current" {}
